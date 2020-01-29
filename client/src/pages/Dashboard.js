ba2ef5a009f3cef5ec06ec7cc8acbfe974ddf3c9
Bigshmow <dstewart88@gmail.com>
2020-01-29 15:26:04 -0800

    count down logic to handle the timeLeft

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -3,6 +3,21 @@ import { Container, Row, Col } from 'reactstrap';
 import { Todocard } from '../components/Todocard';
 import '../styles.css';
 
+const handleTime = () => {
+    const diff = +new Date("2020-05-07") - +new Date();
+    let timeLeft = {};
+
+    if (diff > 0) {
+        timeLeft = {
+            days: Math.floor(diff / (1000 * 60 * 60 * 24)),
+            hours: Math.floor((diff / (1000 * 60 * 60)) % 24 ),
+            minutes: Math.floor((diff / 1000 * 60) % 60),
+            seconds: Math.floor((diff / 1000) % 60)
+        };
+    }
+    return timeLeft;
+}
+
 export const Dashboard = props => {
     return (
         <Container>
