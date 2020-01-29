e85874114e72244d0243c1c674a73db533dee9c1
Bigshmow <dstewart88@gmail.com>
2020-01-28 16:36:00 -0800

    socials updated and slid right, used mr-auto in nav

--- client/src/components/Topnav.js
+++ client/src/components/Topnav.js
@@ -1,13 +1,19 @@
 import React from "react";
-import {Collapse,Navbar,NavbarToggler,NavbarBrand,Nav,NavItem,NavLink} from 'reactstrap';
+import {Nav,Navbar,NavbarBrand,NavItem,NavLink} from 'reactstrap';
 import "../styles.css";
 
 export const Topnav = () => {
   return (
+    <div>
     <Navbar className='shadow' light expand="md">
-      <NavbarBrand href="https://bigshmow.github.io/Portfolio/">
-        {'< DS >'}
-      </NavbarBrand>
+        <NavbarBrand href="https://bigshmow.github.io/Portfolio/">
+          {'< DS >'}
+        </NavbarBrand>
+        <Nav className="mr-auto">
+        </Nav>
+            <NavLink href="https://www.linkedin.com/in/stewart-devin/" className="fa fa-3x fa-linkedin"></NavLink>
+            <NavLink href="https://github.com/Bigshmow" className="fa fa-3x fa-github"></NavLink>
     </Navbar>
+    </div>
   );
 }
