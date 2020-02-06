8cf044546d344f9ac0e26cf3320e038ccbba393a
Bigshmow <dstewart88@gmail.com>
2020-02-06 11:08:24 -0800

    adding changes to server and todos to query correct database collection

--- models/todos.js
+++ models/todos.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const todoSchema = new Schema({
-  name: { type: String, required: true },
-  list: [{ type: String, required: true }]
+  name: { type: String },
+  list: [{ type: String }]
 });
 
-const Todo = mongoose.model("Todo", todoSchema);
+const Todo = mongoose.model("Todos", todoSchema);
 
 module.exports = Todo;
